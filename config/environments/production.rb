516c8043adc69f3595ed8d904508f1b76c2b75e9
Kiter <kiter0211@gmail.com>
2021-07-16 03:51:07 +0000

    Add password reset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,8 +74,8 @@ Rails.application.configure do
     :address        => 'stmp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => ENV['SENDGRID_PASSWORD'],
+    :password       => ENV['SENDGRID_USERNAME'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     root "static_pages#home"
     get "/help", to: "static_pages#help"
@@ -10,5 +12,6 @@ Rails.application.routes.draw do
     delete "/logout", to: "sessions#destroy"
     resources :users
     resources :account_activations, only: [:edit]
+    resources :password_resets, only: [:new, :create, :edit, :update]
   end
 end
