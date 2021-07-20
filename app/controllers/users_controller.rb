d6ec4fe1fc1ce3fe1d8c7ef351cb44c3a712799b
Kiter <kiter0211@gmail.com>
2021-07-20 04:54:54 +0000

    Improve code and use rescue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
+  before_action :find_user, only: :show
   
   def index
     @users = User.paginate(page: params[:page])
@@ -12,9 +13,6 @@ class UsersController < ApplicationController
   end
   
   def show
-    @user = User.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-    redirect_to root_url
   end
   
   def create
@@ -24,27 +22,31 @@ class UsersController < ApplicationController
       flash[:success] = t("static_pages.welcome")
       redirect_to @user
     else 
-      render 'new'
+      render "new"
     end
   end
   
   def edit
-    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = t("users.edit.updated")
       redirect_to @user
     else
-      render 'edit'
+      render "edit"
     end
   end
   
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = t("users.index.deleted")
+    if User.find(params[:id]).destroy
+      flash[:success] = t("users.index.deleted")
+    else
+      flash[:danger] = t("users.index.failed")
+    end
+    redirect_to users_url
+  rescue ActiveRecord::RecordNotFound
+    flash[:danger] = t("global.not_found")
     redirect_to users_url
   end
   
@@ -69,10 +71,19 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    rescue ActiveRecord::RecordNotFound
+      redirect_to root_url
     end
     
     # Confirms an admin user.
     def admin_user
       redirect_to(root_url) unless current_user.admin?
+    end
+    
+    # Find a user
+    def find_user
+      @user = User.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to root_url
     end
 end
